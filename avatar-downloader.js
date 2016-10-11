48f78076f596563926384c77f2c69f81ccab7508
Justin Tsang <justin.b.tsang@gmail.com>
2016-10-11 03:38:46 +0000

    de-linted and fixed syntax

--- avatar-downloader.js
+++ avatar-downloader.js
@@ -1,11 +1,11 @@
 var ownerName = process.argv[2];
-var repoName = process.argv[3]
+var repoName = process.argv[3];
 var request = require("request");
 var fs = require("fs");
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var token = "6296725a4660670cdcfbf7e9b1187e3d3ba2ebbd";
-  var theUrl = "https://api.github.com/repos/" + repoOwner + "/" +repoName + "/contributors"
+  var theUrl = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
   var options = {
     url: theUrl,
     headers: {
@@ -21,22 +21,20 @@ function getRepoContributors(repoOwner, repoName, cb) {
     cb(body);
   }).auth(null, null, true, token);
 }
+function downloadImageByURL(url, filePath) {
+  request(url).pipe(fs.createWriteStream(filePath));
+}
 
 getRepoContributors(ownerName, repoName, function(result){
   fs.mkdir("./avatars/", function(err) {
     if (err) {
-      console.log("Directory Exists!")
+      console.log("Directory Exists!");
     }
     result.forEach(function(user) {
-      var imgUrl = user.avatar_url
+      var imgUrl = user.avatar_url;
       var loginName = user.login;
-      downloadImageByURL(imgUrl, "./avatars/" + loginName + ".jpg")
-      console.log("The image for " + loginName + " at" + imgUrl +" has been downloaded!")
+      downloadImageByURL(imgUrl, "./avatars/" + loginName + ".jpg");
+      console.log("The image for " + loginName + " at" + imgUrl + " has been downloaded!");
     });
   });
 });
-
-
-function downloadImageByURL(url, filePath) {
-  request(url).pipe(fs.createWriteStream(filePath))
-}
